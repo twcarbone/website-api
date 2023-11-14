708bb0b907d834ba62cf2191a9b394d0f4830c27
Tyler Carbone <tcarbone073@gmail.com>
2023-11-14 11:43:37 +0000

    Add protected resource test with valid JWT

--- tests/functional/test_time.py
+++ tests/functional/test_time.py
@@ -3,6 +3,11 @@ def test_time(_client):
     assert response.status_code == 200
 
 
-def test_protected_time(_client):
+def test_protected_time(_client, _existing_user_header_bearer):
+    response = _client.get("/api/protected/time", headers=_existing_user_header_bearer)
+    assert response.status_code == 200
+
+
+def test_protected_time_unauthorized(_client):
     response = _client.get("/api/protected/time")
     assert response.status_code == 401
