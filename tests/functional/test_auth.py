3892e43cc51551bd4e5dece3ea3e34fcadde00d5
Tyler Carbone <tcarbone073@gmail.com>
2023-11-14 15:07:30 +0000

    (Closes #2) Test for correct JWT

--- tests/functional/test_auth.py
+++ tests/functional/test_auth.py
@@ -1,5 +1,32 @@
+import uuid
+
+import flask_jwt_extended
+
 from api import db
 from api import models
+
+# ------------
+# Test helpers
+# ------------
+
+
+def _test_jwt(encoded_token: str):
+    decoded_token = flask_jwt_extended.decode_token(encoded_token=encoded_token)
+
+    assert list(decoded_token.keys()) == ["fresh", "iat", "jti", "type", "sub", "nbf", "exp"]
+
+    assert decoded_token.get("fresh") == False
+    assert decoded_token.get("sub") == 1
+    assert decoded_token.get("type") == "access"
+
+    # Test for valid UUID
+    uuid_str = decoded_token.get("jti")
+    assert str(uuid.UUID(uuid_str)) == uuid_str
+
+
+# -----
+# Tests
+# -----
 
 
 def test_register(_init_db, _client, _new_user_dict):
@@ -28,8 +55,7 @@ def test_register_missing_params(_init_db, _client):
 def test_login(_init_db, _client, _existing_user_dict):
     response = _client.post("api/login", json=_existing_user_dict)
     assert response.status_code == 200
-    # TODO: Test for valid JWT in test_login
-    assert type(response.get_json().get("access_token")) == str
+    _test_jwt(response.json.get("access_token"))
 
 
 def test_login_invalid_password(_init_db, _client, _existing_user_dict):
