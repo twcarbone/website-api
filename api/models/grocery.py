be7f20fdba60ca0cc7b85c488962e913bc8d2d6f
Tyler Carbone <tcarbone073@gmail.com>
2023-11-21 11:28:21 +0000

    Create shoprite receipt parser

--- api/models/grocery.py
+++ api/models/grocery.py
@@ -1,10 +1,19 @@
 from __future__ import annotations
 
+import datetime
 import decimal
+import itertools
+import logging
+import pathlib
+import re
+import typing
+
+import requests
 
 from api import db
 from api.models import Base
 from api.models import date
+from api.models import exc
 from api.models import money
 from api.models import num_6_3
 from api.models import orm
@@ -56,3 +65,142 @@ class ShopriteTransaction(_ShopriteBase):
 class ShopriteDiscount(_ShopriteBase):
     shopritetransaction_id: orm.Mapped[int] = orm.mapped_column(sa.ForeignKey(ShopriteTransaction.id))
     amount: orm.Mapped[money]
+
+
+class ShopriteReceiptParser:
+    MONEY_REGEX = "\$(\d+\.\d{2})"
+
+    @staticmethod
+    def re_group(pattern: str, line: str, cast=str) -> typing.Any:
+        """
+        If *pattern* is found in *line*, return the first matching group cast as *cast*.
+        Otherwise, return an empty string.
+        """
+        try:
+            return cast(re.search(pattern, line).group(1))
+        except AttributeError:  # If no group matched
+            return ""
+
+    @classmethod
+    def serialize_receipt_transactions(cls, receipt: pathlib.Path) -> list[dict]:
+        """
+        Serialize lines in *receipt* file.
+        """
+        with open(receipt, "r") as f:
+            lines = [line.strip() for line in f.readlines()]
+
+        # Join lines for each transaction as comma-separated list
+        groups = [",".join(group) for k, group in itertools.groupby(lines, lambda x: x == "") if not k]
+
+        serialized_receipt_transactions = []
+        for group in groups:
+            serialized = {
+                "name": cls.re_group("^(.*?),", group),
+                "code": cls.re_group(",(\d+),", group),
+                "qty": cls.re_group(f",(\d+) . {cls.MONEY_REGEX},", group, int),
+                "per_qty": cls.re_group(f",\d+ . {cls.MONEY_REGEX},", group, float),
+                "lb": cls.re_group("Qty: (\d+(?:\.\d{1,2})?)lb", group, float),
+                "per_lb": cls.re_group(f"Price: {cls.MONEY_REGEX}/lb", group, float),
+                "discount": cls.re_group(f"\*\*SC\*\*.*{cls.MONEY_REGEX},", group, float),
+                "final": cls.re_group(f",{cls.MONEY_REGEX}.*$", group, float),
+            }
+            serialized_receipt_transactions.append(serialized)
+        return serialized_receipt_transactions
+
+    @staticmethod
+    def commit(row: _ShopriteBase, unique_column: str = None) -> int:
+        """
+        Try to add *row* to session and commit, returning the assigned id of *row*. If a
+        unique constraint violation occurs, query for the row by column *unique_column*.
+        """
+        try:
+            db.session.add(row)
+            db.session.commit()
+            return row.id
+        except exc.IntegrityError:
+            db.session.rollback()
+            mapped = type(row)
+            column = getattr(mapped, unique_column)
+            value = getattr(row, unique_column)
+            return db.session.execute(sa.select(mapped.id).filter(column == value)).scalar_one()
+
+    @staticmethod
+    def fetch(sku: str, store_id: int = 354):
+        """
+        GET product details for *sku* from *store_id* from Wakefern API.
+        """
+        url = f"https://storefrontgateway.brands.wakefern.com/api/stores/{store_id}/products/{sku}"
+        return requests.get(url=url, headers={"x-site-host": "https://www.shoprite.com"})
+
+    @classmethod
+    def details(cls, code: str) -> None | dict:
+        """
+        Return product details, if any are found.
+        """
+        json_data = None
+        if len(code) == 14 and (response := cls.fetch(sku=code)).status_code == 200:
+            json_data = response.json()
+        else:
+            for i in range(10):
+                if (response := cls.fetch(sku=code.zfill(13) + str(i))).status_code == 200:
+                    json_data = response.json()
+                    break
+
+        if json_data is None:
+            logging.warning(f"Did not find details for product code {code}")
+            return
+
+        return {
+            "name": json_data.get("name"),
+            "description": json_data.get("description"),
+            "sku": json_data.get("sku"),
+            "brand": json_data.get("brand"),
+        }
+
+    @classmethod
+    def parse(cls, path: pathlib.Path) -> None:
+        """
+        Parse all receipt files matching the glob pattern *path*.
+        """
+        for receipt in sorted(path.parent.glob(path.stem + path.suffix)):
+            logging.info(f"Parsing receipt '{receipt}' ...")
+            order_id = cls.commit(ShopriteOrder(date=datetime.datetime.strptime(receipt.stem, r"%Y%m%d").date()))
+            for serialized in cls.serialize_receipt_transactions(receipt):
+                product_id = cls.commit(
+                    ShopriteProduct(
+                        name=serialized["name"],
+                        code=serialized["code"],
+                    ),
+                    unique_column="code",
+                )
+
+                transaction_id = cls.commit(
+                    ShopriteTransaction(
+                        shopriteorder_id=order_id,
+                        shopriteproduct_id=product_id,
+                        shopriteratetype_id=1 if serialized["per_lb"] else 2,
+                        rate=serialized["per_lb"] if serialized["per_lb"] else serialized["per_qty"],
+                        qty=serialized["lb"] if serialized["lb"] else serialized["qty"],
+                        final=serialized["final"],
+                    ),
+                )
+
+                if ShopriteProductDetail.scalar_one_or_none(shopriteproduct_id=product_id) is None:
+                    if (details := cls.details(code=serialized["code"])) is not None:
+                        cls.commit(
+                            ShopriteProductDetail(
+                                shopriteproduct_id=product_id,
+                                name=details["name"],
+                                description=details["description"][:500],
+                                brand=details["brand"],
+                                sku=details["sku"],
+                            )
+                        )
+
+                if serialized["discount"]:
+                    cls.commit(
+                        ShopriteDiscount(
+                            shopritetransaction_id=transaction_id,
+                            amount=serialized["discount"],
+                        ),
+                    )
